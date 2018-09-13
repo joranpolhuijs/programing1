09969dc62b6121ea87e4a588329a9cda3efc1f30
joranpolhuijs <joran.polhuijs@student.hu.nl>
2018-09-13 10:36:49 +0200

    les 4 opdracht 3

--- les4/pe4_3.py
+++ les4/pe4_3.py
@@ -1,5 +1,5 @@
 uurloon =  input('wat verdien je per uur: ')
 uren = input('Hoeveel uur werk je: ')
-salaris =  int(uurloon) * int(uren)
+salaris =  float(uurloon) * float(uren)
 line1 = '' + str(uren) + ' uur werken levert je ' + str(salaris) + ' euro op '
 print(line1)
