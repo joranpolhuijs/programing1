0eb690abc08e875cd5dd1cd4bebb9af9cb2704b1
joranpolhuijs <joran.polhuijs@student.hu.nl>
2018-09-13 10:06:11 +0200

    les 4 opdracht 1

--- les4/pe4_1.py
+++ les4/pe4_1.py
@@ -4,5 +4,5 @@ cijferPROG = 6
 totaal = (cijferICOR + cijferCSN + cijferPROG)
 beloning = (totaal* 10)
 gemiddelde = (totaal / 3)
-line1 ='Mijn cijfers (gemiddeld een ' + str(gemiddelde) + ') leveren mij een beloning van ' + str(beloning) + ' euro op.'
-print(line1)
+overzicht ='Mijn cijfers (gemiddeld een ' + str(gemiddelde) + ') leveren mij een beloning van ' + str(beloning) + ' euro op.'
+print(overzicht)
